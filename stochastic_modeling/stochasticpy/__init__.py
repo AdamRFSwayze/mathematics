1369638ce347f75f874a487c97b3a3f3f7731f90
Bobby <me@bobbywlindsey.com>
2016-07-04 22:18:34 -0500

    implemented eigenvector and eigenvalue theory for cat and mouse in house problem; started stochasticpy, a stochastic library for Python

---
+++ stochastic_modeling/stochasticpy/__init__.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sympy as sym
+from math import gcd
+from functools import reduce
+import matplotlib.pyplot as plt
+
+# import classes
+from markov_chain import MarkovChain
+from sample_evolution import SampleEvolution
