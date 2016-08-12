ada16d8d22404aeeb29108325e79d58bb9370404
bobbywlindsey <me@bobbywlindsey.com>
2016-08-12 00:03:13 -0500

    cleaned up directory by archiving files

--- stochastic_modeling/Story Time With Markov Twain/ngram3.jl
+++
@@ -1,7 +0,0 @@
-using JLD
-
-M_3 = load("data/ngram=3/M_3.jld", "M_3")
-M2forM3 = load("data/ngram=3/M2forM3.jld", "M2forM3")
-M1forM3 = load("data/ngram=3/M1forM3.jld", "M1forM3")
-
-transition_matrices = tuple(M_3, M2forM3, M1forM3)
